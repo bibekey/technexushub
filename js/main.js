c6653f96dd46a569db4756239383c8443170455f
BI B EK <127665212+bibekey@users.noreply.github.com>
2026-09-08 09:41:45 +0545

    technexushub

---
+++ js/main.js
@@ -0,0 +1,233 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Main News carousel
+    $(".main-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        center: true,
+    });
+
+
+    // Tranding carousel
+    $(".tranding-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ]
+    });
+
+
+    // Carousel item 1
+    $(".carousel-item-1").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ]
+    });
+
+    // Carousel item 2
+    $(".carousel-item-2").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Carousel item 3
+    $(".carousel-item-3").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+
+    // Carousel item 4
+    $(".carousel-item-4").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+    
+})(jQuery);
+
+//     [ADD BY BIBEK ]
+
+// SMOOTH SCROLL ----------------------------------START
+
+$(document).ready(function() {
+    // Add smooth scroll animation to all anchor links
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function() {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+  
+  //  SMOOTH SCROLL ====================END
+  
+  
+// TABLE OF CONTENT*---------------------------------START
+    // JavaScript function to toggle the table of contents
+  function toggleTableOfContents() {
+    var toc = document.getElementById("table-of-contents");
+    var button = document.getElementById("toggle-button");
+    
+    if (toc.style.display === "none") {
+      toc.style.display = "block"; // Show the table of contents
+      button.textContent = "Table of Contents  [Hide]";
+    } else {
+      toc.style.display = "none"; // Hide the table of contents
+      button.textContent = "Table of Contents  [Show]";
+    }
+  }
+ // TABLE OF CONTENT*=================== END
+ 
+ 
+// NEWSLETTER =================== START
+     function subscribe() {
+      var email = document.getElementById("emailInput").value;
+      var emailRedirect = "mailto:technexushub5@gmail.com?subject=TechNexusHub Newsletter Subscription&body=Email: " + email;
+      window.location.href = emailRedirect;
+    }
+   // NEWSLETTER =================== END
+   
+   
+// NEWSLETTER---------------------------------START
+           const accordions = document.querySelectorAll('.accordion');
+
+        accordions.forEach((accordion) => {
+            accordion.addEventListener('click', () => {
+                accordion.classList.toggle('active');
+
+                const icon = accordion.querySelector('.accordion-icon');
+                if (accordion.classList.contains('active')) {
+                    icon.textContent = '⎺';
+                } else {
+                    icon.textContent = '+';
+                }
+            });
+        });
+     // NEWSLETTER =================== END
+  
+  
+  
+  
+  
+  
